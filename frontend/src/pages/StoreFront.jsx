10d20bdb267bcb91dd5464fa146f3a729d51ad6a
Sbadji29 <sadiabadji9@gmail.com>
2026-02-06 18:09:58 +0000

    style

--- frontend/src/pages/StoreFront.jsx
+++ frontend/src/pages/StoreFront.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import { Sparkle, Stars } from 'lucide-react';
 import Header from '../components/Header';
 import ProductGrid from '../components/ProductGrid';
